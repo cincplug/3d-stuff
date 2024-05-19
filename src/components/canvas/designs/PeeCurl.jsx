c859c69c29b48f2c0b243fbd25bc9bbad6230728
Luka Stanisavljevic <luka.stanisavljevic@cognizant.com>
2024-05-19 12:50:13 +0200

    Improve pee curl

--- src/components/canvas/designs/PeeCurl.jsx
+++ src/components/canvas/designs/PeeCurl.jsx
@@ -7,48 +7,45 @@ import { useControls } from 'leva'
 import { getColorFromIndex } from 'utils'
 const Environment = dynamic(() => import('@/components/canvas/Environment'), { ssr: false })
 
-const Shape = ({ sides, position, index, shapesLength }) => {
-  const ref = React.useRef(null)
-  const color = getColorFromIndex(index, shapesLength)
-
-  const { rodsDensity } = useControls('Shapes', { rodsDensity: { value: 2, min: 1, max: 10, step: 1 } })
-  const { rodsGrowth } = useControls('Shapes', { rodsGrowth: { value: 1, min: -10, max: 10, step: 0.1 } })
-
-  return (
-    <mesh ref={ref} position={position}>
-      <coneGeometry attach='geometry' args={[2, 1, sides]} />
-      <meshStandardMaterial attach='material' color={color} metalness={0.9} roughness={0.5} />
-      {index % rodsDensity === 0 && (
-        <>
-          <coneGeometry attach='geometry' args={[1, index * rodsGrowth, sides]} />
-          <meshStandardMaterial attach='material' color={color} metalness={0.9} roughness={0.7} />
-        </>
-      )}
-    </mesh>
-  )
-}
-
 const PeeCurl = () => {
   const { sides } = useControls('Shapes', { sides: { value: 10, min: 3, max: 30, step: 1 } })
-  const { basesCount } = useControls('Shapes', { basesCount: { value: 100, min: 1, max: 200, step: 1 } })
+  const { count } = useControls('Shapes', { count: { value: 50, min: 20, max: 100, step: 2 } })
   const { spread } = useControls('Shapes', { spread: { value: 0.7, min: 0.1, max: 1.5, step: 0.1 } })
   const { curvature } = useControls('Shapes', { curvature: { value: 1, min: -4, max: 4, step: 0.1 } })
-  const shapes = Array.from({ length: basesCount }, (_, i) => i + 1)
+  const { thickness } = useControls('Shapes', { thickness: { value: 1, min: 0.1, max: 2, step: 0.1 } })
+  const { height } = useControls('Shapes', { height: { value: 20, min: 0.1, max: 50, step: 0.1 } })
+  const { growth } = useControls('Shapes', { growth: { value: 1, min: -10, max: 10, step: 0.1 } })
+
+  const shapes = Array.from({ length: count * 2 }, (_, index) => index + 1)
   return (
     <div className='mx-auto flex size-full flex-col flex-wrap items-center bg-black'>
       <Canvas className='size-full' color='black'>
         <Environment />
-        {shapes.map((_shape, i) => {
-          const angle = (i * Math.PI * 2) / shapes.length
-          const radius = i ** spread
+        {shapes.map((_, index) => {
+          const angle = (index * Math.PI * 2) / shapes.length
+          const radius = index ** spread
+          const position = [radius * Math.cos(angle * curvature), -spread, radius * Math.sin(angle * curvature)]
+          const color = getColorFromIndex(index, shapes.length)
+          const rodHeight = index * growth
+
           return (
-            <Shape
-              key={i}
-              index={i}
-              sides={sides}
-              position={[radius * Math.cos(angle * curvature), 0, radius * Math.sin(angle * curvature)]}
-              shapesLength={shapes.length}
-            />
+            <group key={index} position={position}>
+              {index > 2 && index < shapes.length - 1 && (
+                <mesh position={[0, (growth * height) / 2, 0]}>
+                  <coneGeometry attach='geometry' args={[2, 1, sides]} />
+                  <meshStandardMaterial attach='material' color={color} metalness={0.9} roughness={0.5} />
+                </mesh>
+              )}
+              {index > 0 && index % 2 === 0 && (
+                <mesh position={[0, rodHeight / 2, 0]}>
+                  <cylinderGeometry
+                    attach='geometry'
+                    args={[thickness, thickness, rodHeight + growth * height, sides]}
+                  />
+                  <meshStandardMaterial attach='material' color={color} metalness={0.9} roughness={0.7} />
+                </mesh>
+              )}
+            </group>
           )
         })}
       </Canvas>
