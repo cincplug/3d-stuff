7c776eaaff2e08fd045b2fa42f2a587c79e2fdb3
Luka Stanisavljevic <luka.stanisavljevic@cognizant.com>
2024-05-19 13:11:18 +0200

    Improve bush

--- src/components/canvas/designs/Bush.jsx
+++ src/components/canvas/designs/Bush.jsx
@@ -8,40 +8,31 @@ const Environment = dynamic(() => import('@/components/canvas/Environment'), { s
 
 const Bush = () => {
   const { sides } = useControls('Shapes', { sides: { value: 10, min: 3, max: 30, step: 1 } })
-  const { basesCount } = useControls('Shapes', { basesCount: { value: 100, min: 1, max: 200, step: 1 } })
-  const { density } = useControls('Shapes', { density: { value: 2, min: 1, max: 10, step: 1 } })
-  const shapes = Array.from({ length: basesCount }, (_, i) => i + 1)
+  const { count } = useControls('Shapes', { count: { value: 50, min: 1, max: 200, step: 1 } })
+  const { spread } = useControls('Shapes', { spread: { value: 4, min: -10, max: 10, step: 0.1 } })
+  const { growth } = useControls('Shapes', { growth: { value: 1, min: -10, max: 10, step: 0.1 } })
+  const { thickness } = useControls('Shapes', { thickness: { value: 1, min: -2, max: 3, step: 0.1 } })
+
+  const shapes = Array.from({ length: count }, (_, i) => i + 1)
 
   return (
     <div className='mx-auto flex size-full flex-col flex-wrap items-center bg-black'>
       <Canvas className='size-full' color='black'>
         <Environment />
         {shapes.map((_shape, i) => {
-          const height = Math.sqrt(i + 1)
-          const width = 2 - i / shapes.length
+          const height = Math.sqrt(i + 1) * growth
+          const width = thickness - i / shapes.length
           const depth = i / shapes.length
-          const position = [
-            Math.sin(i) * density,
-            ((i - shapes.length) * height) / shapes.length,
-            Math.cos(i) * density,
-          ]
+          const position = [Math.sin(i) * spread, (i * height) / shapes.length - height / 2, Math.cos(i) * spread]
           return (
-            <group key={i}>
-              <BushBase width={width} depth={depth} height={height} position={position} sides={sides} />
-            </group>
+            <mesh key={i} position={position}>
+              <cylinderGeometry attach='geometry' args={[width * 2, depth, height, sides]} />
+              <meshStandardMaterial attach='material' color='#336600' metalness={0.9} roughness={3.8} />
+            </mesh>
           )
         })}
       </Canvas>
     </div>
-  )
-}
-
-const BushBase = ({ width, height, depth, position, sides }) => {
-  return (
-    <mesh position={position}>
-      <cylinderGeometry attach='geometry' args={[width * 2, depth, height, sides]} />
-      <meshStandardMaterial attach='material' color='#336600' metalness={0.9} roughness={3.8} />
-    </mesh>
   )
 }
 
