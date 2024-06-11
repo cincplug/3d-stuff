6966b9a577a7daf28ce67013153153f0d0d647c9
Luka Stanisavljevic <luka.stanisavljevic@cognizant.com>
2024-06-11 13:25:14 +0200

    Center camera at the beginning

--- src/components/canvas/Environment.jsx
+++ src/components/canvas/Environment.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useThree } from '@react-three/fiber'
+import * as THREE from 'three'
 import { PerspectiveCamera, OrbitControls } from '@react-three/drei'
 import { Color } from 'three'
 
 const Environment = ({ lightness, cameraX, cameraY, cameraZ, bgColor }) => {
+  const { camera, scene } = useThree()
   const cameraPosition = [cameraX, cameraY, cameraZ]
   const threeColor = new Color(bgColor)
+
+  useEffect(() => {
+    const box = new THREE.Box3().setFromObject(scene)
+    const center = box.getCenter(new THREE.Vector3())
+
+    camera.position.x = center.x
+    camera.position.y = center.y
+    camera.position.z = center.z - 20
+
+    camera.lookAt(center)
+  }, [camera, scene])
+
   return (
     <>
       <color attach='background' args={[threeColor.r, threeColor.g, threeColor.b]} />
