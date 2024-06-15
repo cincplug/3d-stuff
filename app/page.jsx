382323b62ed370e333d432ed83d0d785efccdda5
Luka Stanisavljevic <luka.stanisavljevic@cognizant.com>
2024-06-15 11:11:28 +0200

    Make splash nav

--- app/page.jsx
+++ app/page.jsx
@@ -1,43 +1,40 @@
 'use client'
-
 import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
 import { pascalToSpace } from '@/helpers/utils'
 import defaults from '@/variations/defaults'
 import variations from '@/variations'
 import components from '@/components/canvas/ComponentRegistry'
-import Link from 'next/link'
-
-const defaultVariation = 'Muffin'
 
 const extractValues = (defaults) => {
   return Object.fromEntries(Object.entries(defaults).map(([key, { value }]) => [key, value]))
 }
 
-const [defaultComponent, defaultVariationProps] =
-  Object.entries(variations).find(([, vars]) => vars.hasOwnProperty(defaultVariation)) || []
-
-const DefaultComponent = components[defaultComponent]
-
 const Nav = () => {
   const variationKeys = Object.values(variations).flatMap(Object.keys)
   return (
     <div className='absolute grid w-screen grid-cols-4 overflow-auto'>
-      {variationKeys.map((variation, index) => (
-        <Link
-          key={index}
-          href={`/${variation}`}
-          className='flex h-[20vh] items-center justify-center text-2xl text-white'
-        >
-          {pascalToSpace(variation)}
-        </Link>
-      ))}
+      {variationKeys.map((variation, index) => {
+        const [component, variationProps] =
+          Object.entries(variations).find(([, vars]) => vars.hasOwnProperty(variation)) || []
+        const Component = components[component]
+        const initialProps = { ...extractValues(defaults), ...variationProps[variation] }
+        return (
+          <div key={index} className='flex h-[20vh] items-end justify-center text-2xl text-white'>
+            {pascalToSpace(variation)}
+            <div className='mx-auto flex flex-col flex-wrap items-center bg-black'>
+              <Canvas>
+                <Component {...initialProps} />
+              </Canvas>
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
 
 export default function Page() {
-  const initialProps = { ...extractValues(defaults), ...defaultVariationProps[defaultVariation] }
-
   return (
     <div className='mx-auto flex size-full flex-col flex-wrap items-center bg-black'>
       <Suspense fallback={null}>
